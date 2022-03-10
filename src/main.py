a89dbafafa70c846beb031fab18e5c3473e68524
dev-Adri <adrisoftdev@gmail.com>
2022-03-10 22:30:20 +0200

    Automated commit:

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
-print("start")
-print("also this is a test")
+from ursina import *
+
+app = Ursina()
+
+window.title = 'My Game'
+# window.borderless = True
+window.exit_button.visible = True
+window.fps_counter.enabled = True  
+
+cube = Entity(model="cube", color = color.blue, scale=(2, 2, 2))
+
+def update():
+    cube.rotation_y += time.dt * 100
+
+app.run()
