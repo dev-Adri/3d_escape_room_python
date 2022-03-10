a64f733f9ba0a14d915906b0794b502674d264cd
dev-Adri <adrisoftdev@gmail.com>
2022-03-10 22:41:57 +0200

    (TEST: Added some camera movement)

--- src/main.py
+++ src/main.py
@@ -13,6 +13,11 @@ cube = Entity(model="cube", color = color.blue, scale=(2, 2, 2))
 def update():
     cube.rotation_y += time.dt * 100
 
+    if held_keys["q"]:
+        camera.position += (0, time.dt * 3, 0)
+    if held_keys["a"]:
+        camera.position -= (0, time.dt * 3, 0)
+
 def input(key):
     if key == "s":
         cube.color = color.rgb(random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
