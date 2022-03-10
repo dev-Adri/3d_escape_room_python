dd0d706a9d3e7ba07101de2caf59eb30e4ba6943
dev-Adri <adrisoftdev@gmail.com>
2022-03-10 23:26:59 +0200

    (Empty template for now.)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from ursina import *
+from ursina.shaders import lit_with_shadows_shader
 import random
 
 app = Ursina()
@@ -8,18 +9,12 @@ window.title = 'My Game'
 window.exit_button.visible = True
 window.fps_counter.enabled = True  
 
-cube = Entity(model="cube", color = color.blue, scale=(2, 2, 2))
-
 def update():
-    cube.rotation_y += time.dt * 100
-
-    if held_keys["q"]:
-        camera.position += (0, time.dt * 3, 0)
-    if held_keys["a"]:
-        camera.position -= (0, time.dt * 3, 0)
+    pass
 
 def input(key):
-    if key == "s":
-        cube.color = color.rgb(random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
+    pass
+    # if key == "s":
+    #     cube.color = color.rgb(random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
 
 app.run()
